9de7c92558304af2f19241eaa370f63326f97144
Abhishek-Rath0re <ranaabhi0611@gmail.com>
2025-11-11 06:51:14 +0000

    string methods

---
+++ basic/string.js
@@ -0,0 +1,12 @@
+const name="Abhishek"
+const repoCount=50
+let text=("Amar Pandat is Good Boy")
+let newText=text.replace('Pandat','Sharma'
+)
+console.log(newText)//thats how the replace tag is work
+//for changing the url we use the method .replace
+let url='https//abhishek%20rathore/abhishek.com'
+console.log(url.replace('%20','__'));
+//.slice is also a method of string (It use to write a number between -ive idex to +ive idex)
+console.log(name.slice(-8,4));
+//There is more methods of strings that is (read it from the w3 shool website)
