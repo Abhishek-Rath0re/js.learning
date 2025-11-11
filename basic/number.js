5a6fdd90a0059b95cf30daeb9360058029258541
Abhishek-Rath0re <ranaabhi0611@gmail.com>
2025-11-11 07:24:02 +0000

    number and math

---
+++ basic/number.js
@@ -0,0 +1,31 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(100)
+// console.log(balance);
+//.toFixed is used to fixed the number it means at the end add to zero(100.00)
+//console.log(score.toFixed(2))
+//.toPrecision is use to precise the value of no. example 123.75889 it precise it like 123.8
+//.toPrecise(3) is use in it 
+//.toLocalString is use to write a comma's in the number ****Example**
+// const digit=20000000
+// console.log(digit.toLocaleString)
+//*************Maths***********/
+console.log(Math)
+console.log(Math.abs(-4))
+//it connvert the -ive no. into +ive no. not reversable
+console.log(Math.round(4.55));//output=5
+//It round of the value
+console.log(Math.ceil(4.2));//Output=4
+//It round of the value in maximum of the number
+console.log(Math.floor(4.8));//Output=4
+//It round off the value to minimum
+console.log(Math.min(4,8,9,5,4,2,7,9))//Output=2
+//It is use to find the minimum value of array
+//same work Math.max to find the maximum no. in array
+console.log(Math.random())//It pick the random value between 0 to1
+const min=10
+const max=20
+console.log(Math.floor((Math.random()*(max-min+1)+min)))
+
+
+
